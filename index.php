02af48c6437ea35565e51024762686fb1d9a1e96
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-23 08:33:40 -0600

    committed attempt at snap-challenge factorials

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+function factorialOfANumber($n) {
+	if(n! = n * (n -1) * (n - 2) * 3 * 3 *1)
+		catch (\RangeException $exception) {
+			throw (RangeException ($exception->getMessage(), 0, $exception))
+	}
+		return (n! * factorialOfANumber($n- 1)) {
+		if($n > 0)
+			throw(new\RangeException("Cannot have negatives"))
+	}
+	return (n! * factorialOfANumber($n- 1)){
+		if($n < 20)
+			throw(new\RangeException("Number too high"))
+			}
+return($n)
+}
